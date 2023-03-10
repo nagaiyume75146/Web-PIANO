3fd9e3526247a854669549950b1e6d09fe7b5e85
nagaiyume75146 <nagaiyume75146@gmail.com>
2023-03-11 04:31:28 +0900

    更新しました

--- src/sp/sp.js
+++ src/sp/sp.js
@@ -11,15 +11,21 @@
   ・boolean系(true/false):is〇〇(受動態)
 */
 
-/*
-// ロードしたときに画面を合わせる
-window.addEventListener("load",function (params) {
-  
-})
+alert("画面を固定してください")
 
-// リサイズしたときに画面を合わせる
-window.addEventListener("resize",function () {
-})*/
+// ロードしたときに画面を合わせる
+window.addEventListener("load",function () {
+  const keyboard = document.getElementsByClassName("keyboard")[0];
+  root.style.setProperty("--height", height);
+  
+  const root = document.querySelector(':root');
+
+  const bottom = document.getElementsByClassName("bottom")[0];
+  
+
+  const height = window.innerHeight + "px";
+  root.style.setProperty("--height", height);
+})
 
 let touchkey = {}; // タッチしたキーを保存する配列
 let audio = {}; // 音を保存する配列
