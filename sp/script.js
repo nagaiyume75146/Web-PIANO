f0dc5ccacac3196282c25ccf22a3355a44171059
nagaiyume75146 <nagaiyume75146@gmail.com>
2023-03-11 21:05:32 +0900

    更新しました

--- sp/script.js
+++ sp/script.js
@@ -4,17 +4,8 @@ window.addEventListener("load", function() {
     const piano = document.getElementsByClassName("piano")[0];
     piano.style.width = window.innerWidth + "px";
     piano.style.height = window.innerHeight + "px";
-})
 
-window.addEventListener("resize", function() {
-    const piano = document.getElementsByClassName("piano")[0];
-
-    piano.style.width = window.innerWidth + "px";
-    piano.style.height = window.innerHeight + "px";
-})
-
-/*
-const bottom = document.getElementsByClassName("bottom")[0];
+    const bottom = document.getElementsByClassName("bottom")[0];
     const keyboard = document.getElementsByClassName("keyboard")[0];
     if( ( bottom.innerWidth / bottom.innerHeigh ) > ( 880 / 220 ) ) {
         keyboard.style.width = "";
@@ -24,4 +15,22 @@ const bottom = document.getElementsByClassName("bottom")[0];
         keyboard.style.width = "100%";
         keyboard.style.height = "";
     }
-*/
+})
+
+window.addEventListener("resize", function() {
+    const piano = document.getElementsByClassName("piano")[0];
+
+    piano.style.width = window.innerWidth + "px";
+    piano.style.height = window.innerHeight + "px";
+
+    const bottom = document.getElementsByClassName("bottom")[0];
+    const keyboard = document.getElementsByClassName("keyboard")[0];
+    if( ( bottom.innerWidth / bottom.innerHeigh ) > ( 880 / 220 ) ) {
+        keyboard.style.width = "";
+        keyboard.style.height = "100%";
+    }
+    else {
+        keyboard.style.width = "100%";
+        keyboard.style.height = "";
+    }
+})
